97529956f0cd97e680b3d305c539daa1db686371
Sam Kohn <samjkohn@gmail.com>
2024-10-15 01:38:22 -0700

    Start gathering view definitions

--- pg_update_dependents/object_graph.py
+++ pg_update_dependents/object_graph.py
@@ -1,10 +1,11 @@
 import argparse
 import dataclasses
 import pprint
-import typing
+from typing import Literal
 
-import pg8000.native
 import networkx as nx
+
+import sql
 
 
 @dataclasses.dataclass(frozen=True)
@@ -18,52 +19,13 @@ class SQLObject:
         return self.schema + "." + self.name
 
 
-def get_sql_objects_raw(user, host, database, port, password) -> list[list[str]]:
-    conn = pg8000.native.Connection(user, host, database, port, password)
-    raw_sql_results = conn.run(
-        """WITH RECURSIVE view_deps AS (
-SELECT DISTINCT dependent_ns.nspname as dependent_schema
-, dependent_view.relname as dependent_view
-, dependent_view.oid as dependent_oid
-, dependent_view.relkind as dependent_relkind
-, source_ns.nspname as source_schema
-, source_table.relname as source_table
-, source_table.oid as source_oid
-, source_table.relkind as source_relkind
-FROM pg_depend
-JOIN pg_rewrite ON pg_depend.objid = pg_rewrite.oid
-JOIN pg_class as dependent_view ON pg_rewrite.ev_class = dependent_view.oid
-JOIN pg_class as source_table ON pg_depend.refobjid = source_table.oid
-JOIN pg_namespace dependent_ns ON dependent_ns.oid = dependent_view.relnamespace
-JOIN pg_namespace source_ns ON source_ns.oid = source_table.relnamespace
-WHERE NOT (dependent_ns.nspname = source_ns.nspname AND dependent_view.relname = source_table.relname)
-UNION
-SELECT DISTINCT dependent_ns.nspname as dependent_schema
-, dependent_view.relname as dependent_view
-, dependent_view.oid as dependent_oid
-, dependent_view.relkind as dependent_relkind
-, source_ns.nspname as source_schema
-, source_table.relname as source_table
-, source_table.oid as source_oid
-, source_table.relkind as source_relkind
-FROM pg_depend
-JOIN pg_rewrite ON pg_depend.objid = pg_rewrite.oid
-JOIN pg_class as dependent_view ON pg_rewrite.ev_class = dependent_view.oid
-JOIN pg_class as source_table ON pg_depend.refobjid = source_table.oid
-JOIN pg_namespace dependent_ns ON dependent_ns.oid = dependent_view.relnamespace
-JOIN pg_namespace source_ns ON source_ns.oid = source_table.relnamespace
-INNER JOIN view_deps vd
-    ON vd.dependent_schema = source_ns.nspname
-    AND vd.dependent_view = source_table.relname
-    AND NOT (dependent_ns.nspname = vd.dependent_schema AND dependent_view.relname = vd.dependent_view)
-)
-SELECT *
-FROM view_deps
-ORDER BY source_schema, source_table;
-"""
-    )
+@dataclasses.dataclass
+class Action:
+    action_type: Literal["drop"] | Literal["create"] | Literal["create-target"]
+    obj: SQLObject
 
-    return raw_sql_results
+    def to_string(self) -> str:
+        return self.action_type + " " + self.obj.to_string()
 
 
 def raw_to_edge(raw_row: list) -> tuple[SQLObject, SQLObject]:
@@ -77,19 +39,34 @@ def raw_to_edge(raw_row: list) -> tuple[SQLObject, SQLObject]:
 def create_edge_list(raw_sql_results: list) -> list[tuple[SQLObject, SQLObject]]:
     return [raw_to_edge(row) for row in raw_sql_results]
 
+
+def order(graph, target) -> list[Action]:
+    subgraph = graph.subgraph(nx.descendants(graph, target))
+    topo_order = list(nx.topological_sort(subgraph))
+    drops = [Action("drop", node) for node in reversed(topo_order)]
+    target_actions = [Action("drop", target), Action("create-target", target)]
+    creates = [Action("create", node) for node in topo_order]
+    return drops + target_actions + creates
+
+
 def main(args):
-    raw = get_sql_objects_raw(args.user, args.host, args.db, args.port, args.password)
+    conn = sql.get_conn(args.user, args.host, args.db, args.port, args.password)
+    raw = sql.get_sql_objects_raw(conn)
     edge_list = create_edge_list(raw)
     graph = nx.DiGraph(edge_list)
-    test_val = SQLObject('public', 'person_current_sr', 'v', 667242)
-    pprint.pprint(dict(graph[test_val]))
-    pprint.pprint(list(graph.successors(test_val)))
-    pprint.pprint(list(graph.predecessors(test_val)))
-    print(graph.in_degree(test_val))
-    print(graph.out_degree(test_val))
-    print(nx.is_directed_acyclic_graph(graph))
-    subgraph = graph.subgraph(nx.descendants(graph, test_val))
-    pprint.pprint(list(nx.topological_sort(subgraph)))
+    test_val = SQLObject("public", "person_current_sr", "v", 667242)
+    # pprint.pprint([action.to_string() for action in order(graph, test_val)])
+    pprint.pprint(
+        sql.retrieve_definitions(
+            conn,
+            [
+                action.obj
+                for action in order(graph, test_val)
+                if "create" in action.action_type
+            ][:5]
+        )
+    )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
---
+++ pg_update_dependents/sql.py
@@ -0,0 +1,72 @@
+import pg8000.native
+
+def get_conn(user, host, database, port, password) -> pg8000.native.Connection:
+    return pg8000.native.Connection(user, host, database, port, password)
+
+def get_sql_objects_raw(conn) -> list[list[str]]:
+    raw_sql_results = conn.run(
+        """WITH RECURSIVE view_deps AS (
+SELECT DISTINCT dependent_ns.nspname as dependent_schema
+, dependent_view.relname as dependent_view
+, dependent_view.oid as dependent_oid
+, dependent_view.relkind as dependent_relkind
+, source_ns.nspname as source_schema
+, source_table.relname as source_table
+, source_table.oid as source_oid
+, source_table.relkind as source_relkind
+FROM pg_depend
+JOIN pg_rewrite ON pg_depend.objid = pg_rewrite.oid
+JOIN pg_class as dependent_view ON pg_rewrite.ev_class = dependent_view.oid
+JOIN pg_class as source_table ON pg_depend.refobjid = source_table.oid
+JOIN pg_namespace dependent_ns ON dependent_ns.oid = dependent_view.relnamespace
+JOIN pg_namespace source_ns ON source_ns.oid = source_table.relnamespace
+WHERE NOT (dependent_ns.nspname = source_ns.nspname AND dependent_view.relname = source_table.relname)
+UNION
+SELECT DISTINCT dependent_ns.nspname as dependent_schema
+, dependent_view.relname as dependent_view
+, dependent_view.oid as dependent_oid
+, dependent_view.relkind as dependent_relkind
+, source_ns.nspname as source_schema
+, source_table.relname as source_table
+, source_table.oid as source_oid
+, source_table.relkind as source_relkind
+FROM pg_depend
+JOIN pg_rewrite ON pg_depend.objid = pg_rewrite.oid
+JOIN pg_class as dependent_view ON pg_rewrite.ev_class = dependent_view.oid
+JOIN pg_class as source_table ON pg_depend.refobjid = source_table.oid
+JOIN pg_namespace dependent_ns ON dependent_ns.oid = dependent_view.relnamespace
+JOIN pg_namespace source_ns ON source_ns.oid = source_table.relnamespace
+INNER JOIN view_deps vd
+    ON vd.dependent_schema = source_ns.nspname
+    AND vd.dependent_view = source_table.relname
+    AND NOT (dependent_ns.nspname = vd.dependent_schema AND dependent_view.relname = vd.dependent_view)
+)
+SELECT *
+FROM view_deps
+ORDER BY source_schema, source_table;
+"""
+    )
+
+    return raw_sql_results
+
+
+def definition_query(obj) -> str:
+    if obj.kind == "m":
+        return f"""SELECT
+    definition
+FROM
+    pg_matviews
+WHERE
+    schemaname = {pg8000.native.literal(obj.schema)}
+    AND
+    matviewname = {pg8000.native.literal(obj.name)};"""
+    elif obj.kind == "v":
+        return f"SELECT pg_get_viewdef({pg8000.native.literal(obj.oid)}, true);"
+    else:
+        raise ValueError(f"Invalid SQLObject kind: {obj!r}")
+
+
+def retrieve_definitions(conn: pg8000.native.Connection, objs) -> list[str]:
+    query = [definition_query(obj) for obj in objs]
+    result = conn.run("\n".join(query))
+    return result
