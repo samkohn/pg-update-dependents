66194e5f9bf5feed55bbe6054d227f8e4920664b
Sam Kohn <sam@uaw4811.org>
2025-05-30 11:49:56 -0700

    Tweak formatting of WITH NO DATA

--- src/postgresql_dependent_objects/sql.py
+++ src/postgresql_dependent_objects/sql.py
@@ -60,7 +60,7 @@ def definition_query(obj, with_no_data) -> str:
         return f"""SELECT
     'CREATE MATERIALIZED VIEW {obj.str_safe()}'
     || E'\\nAS' || {"definition" if not with_no_data
-    else "RTRIM(definition, ';') || ' WITH NO DATA;'"}
+    else "RTRIM(definition, ';') || '\nWITH NO DATA;'"}
     || CASE
         WHEN indexes.indexdefs IS NULL THEN ''
         ELSE E'\\n\\n' || indexes.indexdefs
